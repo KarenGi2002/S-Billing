0945ba779816c56356df8b9964bd89fcd0220a07
jorgeabrahan <jorge24abrahan@gmail.com>
2023-06-01 00:50:13 -0600

    Create main jsx file and setup routes

---
+++ src/main.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { Navbar } from './facturation/components'
+import { App, ErrorPage } from './facturation/pages'
+import { Login, Signup } from './auth/pages'
+import { Articles } from './facturation/pages/article/Articles'
+import { Clients } from './facturation/pages/client/Clients'
+import { Inventories } from './facturation/pages/inventory/Inventories'
+import { Invoices } from './facturation/pages/invoice/Invoices'
+import { Reports } from './facturation/pages/report/Reports'
+import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Navbar />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <App />
+      },
+      {
+        path: '/articles',
+        element: <Articles />
+      },
+      {
+        path: '/clients',
+        element: <Clients />
+      },
+      {
+        path: '/inventories',
+        element: <Inventories />
+      },
+      {
+        path: '/invoices',
+        element: <Invoices />
+      },
+      {
+        path: '/reports',
+        element: <Reports />
+      }
+    ]
+  },
+  {
+    path: '/login',
+    element: <Login />
+  },
+  {
+    path: '/signup',
+    element: <Signup />
+  }
+])
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+)
