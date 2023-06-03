a5972c59de739089f681b3b893c6b74f7a37af99
jorgeabrahan <jorge24abrahan@gmail.com>
2023-06-03 15:20:58 -0600

    Get report ID and invoice ID using url params on the invoice component

--- src/facturation/pages/invoice/Invoice.jsx
+++ src/facturation/pages/invoice/Invoice.jsx
@@ -1,7 +1,12 @@
+import { useParams } from 'react-router-dom'
+
 export const Invoice = () => {
+  const { report_id, invoice_id } = useParams()
   return (
     <>
-      <h1>Invoice</h1>
+      <h1>
+        Invoice: {invoice_id} for report: {report_id}
+      </h1>
       <p>/reports/:report_id/invoices/:invoice_id</p>
     </>
   )
