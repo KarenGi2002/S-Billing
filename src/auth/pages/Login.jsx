3ba342d1e711e31f506ae225a9e609991e411d18
jorgeabrahan <jorge24abrahan@gmail.com>
2023-06-03 15:18:01 -0600

    Create form styles using antd components

--- src/auth/pages/Login.jsx
+++ src/auth/pages/Login.jsx
@@ -1,25 +1,57 @@
 import { useNavigate, NavLink } from 'react-router-dom'
+import { Button, Form, Input } from 'antd'
 import './auth.css'
 
 export const Login = () => {
   const navigate = useNavigate()
-  const onLogin = () => {
+  const [form] = Form.useForm()
+  const onFinish = () => {
     navigate('/', {
       replace: true
     })
   }
+
   return (
     <section className="auth">
       <div className="auth__container">
-        <h1>Welcome to S-Billing</h1>
-        <form className="auth__form">
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
-          <NavLink to="/signup">Don&apos;t have an account?</NavLink>
-          <button type="submit" onClick={onLogin}>
-            Log in
-          </button>
-        </form>
+        <Form form={form} layout="vertical" onFinish={onFinish} autoComplete="off">
+          <Form.Item
+            label="Email"
+            name="email"
+            required
+            tooltip="This is a required field"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your email!'
+              }
+            ]}
+          >
+            <Input placeholder="e.g.: email@gmail.com" type="email" />
+          </Form.Item>
+          <Form.Item
+            label="Password"
+            name="password"
+            required
+            tooltip="This is a required field"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your password!'
+              }
+            ]}
+          >
+            <Input.Password placeholder="e.g.: safepassword" />
+          </Form.Item>
+          <Form.Item style={{ marginBottom: 0 }}>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+            <Button type="link">
+              <NavLink to="/signup">Don&apos;t have an account?</NavLink>
+            </Button>
+          </Form.Item>
+        </Form>
       </div>
     </section>
   )
--- src/auth/pages/Signup.jsx
+++ src/auth/pages/Signup.jsx
@@ -1,8 +1,10 @@
+import { Button, Form, Input } from 'antd'
 import { useNavigate, NavLink } from 'react-router-dom'
 
 export const Signup = () => {
   const navigate = useNavigate()
-  const onSignup = () => {
+  const [form] = Form.useForm()
+  const onFinish = () => {
     navigate('/', {
       replace: true
     })
@@ -10,16 +12,58 @@ export const Signup = () => {
   return (
     <section className="auth">
       <div className="auth__container">
-        <h1>Register to S-Billing</h1>
-        <form className="auth__form">
-          <input type="text" placeholder="Username" />
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
-          <NavLink to="/login">Already have an account?</NavLink>
-          <button type="submit" onClick={onSignup}>
-            Sign up
-          </button>
-        </form>
+        <Form form={form} layout="vertical" onFinish={onFinish} autoComplete="off">
+          <Form.Item
+            label="Username"
+            name="username"
+            required
+            tooltip="This is a required field"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your username!'
+              }
+            ]}
+          >
+            <Input placeholder="e.g.: username" />
+          </Form.Item>
+          <Form.Item
+            label="Email"
+            name="email"
+            required
+            tooltip="This is a required field"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your email!'
+              }
+            ]}
+          >
+            <Input placeholder="e.g.: email@gmail.com" type="email" />
+          </Form.Item>
+          <Form.Item
+            label="Password"
+            name="password"
+            required
+            tooltip="This is a required field"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your password!'
+              }
+            ]}
+          >
+            <Input.Password placeholder="e.g.: safepassword" />
+          </Form.Item>
+          <Form.Item style={{ marginBottom: 0 }}>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+            <Button type="link">
+              <NavLink to="/login">Already have an account?</NavLink>
+            </Button>
+          </Form.Item>
+        </Form>
       </div>
     </section>
   )
