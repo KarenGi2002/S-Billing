1237351c9b2614c942b610cad27d72530efba224
jorgeabrahan <jorge24abrahan@gmail.com>
2023-06-03 15:19:02 -0600

    Get inventory ID from the articles component using url params

--- src/facturation/pages/article/Articles.jsx
+++ src/facturation/pages/article/Articles.jsx
@@ -1,7 +1,10 @@
+import { useParams } from 'react-router-dom'
+
 export const Articles = () => {
+  const { inventory_id } = useParams()
   return (
     <>
-      <h1>Articles</h1>
+      <h1>Articles for inventory {inventory_id}</h1>
       <p>/inventories/:inventory_id/articles</p>
     </>
   )
