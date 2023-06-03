dcf6e213ce2424867e2b0b783cfacae04d6d8807
jorgeabrahan <jorge24abrahan@gmail.com>
2023-06-03 15:18:39 -0600

    Create styles for navbar using antd components

--- src/facturation/components/Navbar.jsx
+++ src/facturation/components/Navbar.jsx
@@ -1,14 +1,43 @@
 import { Outlet, NavLink } from 'react-router-dom'
+import { Menu } from 'antd'
+import { useState } from 'react'
+
+const items = [
+  {
+    label: <NavLink to="/inventories">Inventories</NavLink>,
+    key: 'inventories',
+    icon: <span className="material-symbols-outlined">inventory</span>
+  },
+  {
+    label: <NavLink to="/clients">Clients</NavLink>,
+    key: 'clients',
+    icon: <span className="material-symbols-outlined">group</span>
+  },
+  {
+    label: <NavLink to="/reports">Reports</NavLink>,
+    key: 'reports',
+    icon: <span className="material-symbols-outlined">lab_profile</span>
+  },
+  {
+    label: <NavLink to="/login">Logout</NavLink>,
+    key: 'login',
+    icon: <span className="material-symbols-outlined">logout</span>,
+    danger: true
+  }
+]
 
 export const Navbar = () => {
+  const [current, setCurrent] = useState('inventories')
+  const onClick = (e) => setCurrent(e.key)
   return (
     <>
-      <nav>
-        <NavLink to="/inventories">Inventories</NavLink>
-        <NavLink to="/clients">Clients</NavLink>
-        <NavLink to="/reports">Reports</NavLink>
-      </nav>
-
+      <Menu
+        onClick={onClick}
+        selectedKeys={[current]}
+        mode="horizontal"
+        items={items}
+        theme="dark"
+      />
       <Outlet />
     </>
   )
