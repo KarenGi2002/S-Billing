d56a754b0025fcbd1ae1d6d34e7bf7179879e1ee
jorgeabrahan <jorge24abrahan@gmail.com>
2023-06-08 00:23:40 -0600

    Display button to add a new client and a form to add it when the button gets clicked

--- src/facturation/pages/client/Clients.jsx
+++ src/facturation/pages/client/Clients.jsx
@@ -1,7 +1,30 @@
+import { Button } from 'antd'
+import { useState } from 'react'
+import { AddClient } from '../../components/client/AddClient'
+
 export const Clients = () => {
+  const [showAddClientForm, setShowAddClientForm] = useState(false)
+  const toggleShowAddClientForm = () => setShowAddClientForm((prev) => !prev)
+
   return (
-    <>
-      <h1>Clients</h1>
-    </>
+    <section className="container">
+      <Button
+        type="primary"
+        size="large"
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          gap: '0.2rem',
+          marginLeft: 'auto',
+          marginBlock: '1rem'
+        }}
+        onClick={toggleShowAddClientForm}
+      >
+        New
+        <span className="material-symbols-outlined">add</span>
+      </Button>
+
+      {showAddClientForm && <AddClient toggleShowAddClientForm={toggleShowAddClientForm} />}
+    </section>
   )
 }
