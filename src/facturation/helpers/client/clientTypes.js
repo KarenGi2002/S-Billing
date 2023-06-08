ef17a3986fa750033643fa0790247e8e36edce51
jorgeabrahan <jorge24abrahan@gmail.com>
2023-06-08 10:05:28 -0600

    Move the client types array into a client helpers folder

---
+++ src/facturation/helpers/client/clientTypes.js
@@ -0,0 +1,15 @@
+const clientTypes = [
+  {
+    value: 'Normal',
+    label: 'Normal'
+  },
+  {
+    value: 'Wholesale',
+    label: 'Wholesale'
+  },
+  {
+    value: 'Internal',
+    label: 'Internal'
+  }
+]
+export { clientTypes }
