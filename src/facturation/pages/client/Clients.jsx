809ec028a229831bf58e168a40ac2d0b14f18724
jorgeabrahan <jorge24abrahan@gmail.com>
2023-06-08 01:23:43 -0600

    Implement add new button component in clients page

--- src/facturation/pages/client/Clients.jsx
+++ src/facturation/pages/client/Clients.jsx
@@ -1,30 +1,14 @@
-import { Button } from 'antd'
 import { useState } from 'react'
-import { AddClient } from '../../components/client/AddClient'
+import { AddClient, AddNewButton } from '../../components/'
 
 export const Clients = () => {
-  const [showAddClientForm, setShowAddClientForm] = useState(false)
-  const toggleShowAddClientForm = () => setShowAddClientForm((prev) => !prev)
+  const [displayForm, setDisplayForm] = useState(false)
+  const toggleDisplayForm = () => setDisplayForm((prev) => !prev)
 
   return (
     <section className="container">
-      <Button
-        type="primary"
-        size="large"
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          gap: '0.2rem',
-          marginLeft: 'auto',
-          marginBlock: '1rem'
-        }}
-        onClick={toggleShowAddClientForm}
-      >
-        New
-        <span className="material-symbols-outlined">add</span>
-      </Button>
-
-      {showAddClientForm && <AddClient toggleShowAddClientForm={toggleShowAddClientForm} />}
+      <AddNewButton toggleFormPopup={toggleDisplayForm} />
+      {displayForm && <AddClient toggleDisplayForm={toggleDisplayForm} />}
     </section>
   )
 }
