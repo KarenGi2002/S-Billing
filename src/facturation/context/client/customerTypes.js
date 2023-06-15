74fb4d4f3e8db5627c36bf4d3855c226741d7ca5
jorgeabrahan <jorge24abrahan@gmail.com>
2023-06-15 14:55:45 -0600

    Create customer types for the client reducer

---
+++ src/facturation/context/client/customerTypes.js
@@ -0,0 +1,9 @@
+export const customerTypes = {
+  setCurrent: '[Customer] Set current',
+  setFoundCurrent: '[Customer] Set found current',
+  clearCurrent: '[Customer] Clear current',
+  remove: '[Customer] Remove customer',
+  add: '[Customer] Append customer',
+  update: '[Customer] Update customer',
+  setAll: '[Customer] Set customers'
+}
