cf2fea361c761e0300a0c7c24c68bcf7c0e3c4ce
hnkatze <henriquezhector1@hotmail.com>
2023-06-16 23:07:53 -0600

    add a new export of Inventory

--- src/facturation/components/index.js
+++ src/facturation/components/index.js
@@ -7,3 +7,6 @@ export * from './client/EditClient'
 
 export * from './product/AddProduct'
 export * from './product/EditProduct'
+
+export* from  './inventory/AddInventory'
+export* from  './inventory/EditInventory'
