44785aa64208d240411cd9f14da9c5935258ceb6
hnkatze <henriquezhector1@hotmail.com>
2023-06-20 21:15:29 -0600

    Create a new helper

---
+++ src/facturation/helpers/article/deleteArticle.js
@@ -0,0 +1,21 @@
+import { ArticleApi } from '../../../services'
+
+const deleteArticle = (articleId, setArticles, messageApi) => {
+  new ArticleApi()
+    .apiArticleIdDelete(articleId)
+    .then(() => {
+      setArticles((prev) => prev.filter((obj) => obj.articleId !== articleId))
+      messageApi.open({
+        type: 'success',
+        content: 'Article has been delete successfully!',
+      });
+    })
+    .catch(() => {
+      messageApi.open({
+        type: 'success',
+        content: 'Article hasn\'t been delete error!',
+      });
+    })
+}
+
+export { deleteArticle }
